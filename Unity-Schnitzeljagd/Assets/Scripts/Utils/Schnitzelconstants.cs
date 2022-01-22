d5bd4f9fc08dac9e67f23789ec47eaf3ebd50b80
Konstantin Karas <konstantin.karas@tum.de>
2022-01-22 16:29:23 +0100

    added townhall scene

--- Unity-Schnitzeljagd/Assets/Scripts/Utils/Schnitzelconstants.cs
+++ Unity-Schnitzeljagd/Assets/Scripts/Utils/Schnitzelconstants.cs
@@ -6,5 +6,5 @@ public static class Schnitzelconstants
 {
     public static string WORLD_SCENE = "MunichMap";
     public static string MAXBURG_SCENE = "test";
-    public static string OLD_TOWNHALL_SCENE = "Old Town Hall";
+    public static string OLD_TOWNHALL_SCENE = "OldTownHall";
 }
