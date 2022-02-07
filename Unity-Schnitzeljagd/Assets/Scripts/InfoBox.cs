2703cdf602b099c1fd8b1c4f241a4b0eda9cf8f3
Lara <laraemelie.li@gmail.com>
2022-02-07 17:42:46 +0100

    dialogue system changed + sendlinger tor pic added

--- Unity-Schnitzeljagd/Assets/Scripts/InfoBox.cs
+++ Unity-Schnitzeljagd/Assets/Scripts/InfoBox.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,19 @@ using UnityEngine;
 public class InfoBox : MonoBehaviour
 {
     public int id;
+
+    private GameObject dialogeBox;
+    private DialogueSystem diaSys;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dialogeBox = GameObject.Find("Dialogue Box");
+        diaSys = dialogeBox.GetComponent<DialogueSystem>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        diaSys.StartDialogue(id);
     }
 }
