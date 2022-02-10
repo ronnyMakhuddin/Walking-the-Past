9f167dfa4a04dd71a5d0e09e87ba0890748e1f52
Konstantin Karas <konstantin.karas@tum.de>
2022-02-10 19:20:34 +0100

    Auto stash before merge of "hackathon" and "origin/hackathon"

--- Unity-Schnitzeljagd/Assets/Scripts/Utils/MoveForARSetup.cs
+++ Unity-Schnitzeljagd/Assets/Scripts/Utils/MoveForARSetup.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using UnityEngine.SceneManagement;
+
 
 public class MoveForARSetup : MonoBehaviour
 {
+    private bool isTownhall = false; 
     private Vector3 scale_real_to_map = new Vector3(-2f, 1f, -2f);
     void Awake()
     {
+        isTownhall = SceneManager.GetActiveScene().Equals(SceneManager.GetSceneByName(Schnitzelconstants.OLD_TOWNHALL_SCENE));
+        Debug.Log(isTownhall);
+
+        if (isTownhall) scale_real_to_map = Vector3.Scale(scale_real_to_map, new Vector3(-1f, 1f, -1f));
+
         bool isCam = gameObject.tag.Equals("ARSessionOrigin");
         Vector3 relativePos = GameManager.Instance.arOriginRelativeToPlayer;
         Vector3 absPos = isCam ? GameManager.Instance.playerPos : GameManager.Instance.arOriginPos;
@@ -17,9 +25,15 @@ public class MoveForARSetup : MonoBehaviour
             if (GameManager.Instance.useAbsolutePos)
             {
                 transform.position = Vector3.Scale(new Vector3(absPos.x, 1.3f, absPos.z), scale_real_to_map);
-
-                Vector3 playerRot = GameManager.Instance.playerOrientation.eulerAngles;
-                transform.Rotate(new Vector3(0f, playerRot.y + 180f, 0f)); // player's y direction is inverted on mapbox!
+                if (isTownhall)
+                {
+                    transform.rotation = Quaternion.Inverse(GameManager.Instance.playerOrientation);
+                }
+                else
+                {
+                    Vector3 playerRot = GameManager.Instance.playerOrientation.eulerAngles;
+                    transform.Rotate(new Vector3(0f, playerRot.y + 180f, 0f)); // player's y direction is inverted on mapbox!
+                }
             }
         }else
         {
@@ -27,7 +41,19 @@ public class MoveForARSetup : MonoBehaviour
             if (GameManager.Instance.useAbsolutePos)
             {
                 transform.position = Vector3.Scale(new Vector3(absPos.x, 0, absPos.z), scale_real_to_map);
-                transform.rotation = Quaternion.Inverse(GameManager.Instance.arAnchorOrientation);
+                Vector3 playerRot = GameManager.Instance.playerOrientation.eulerAngles;
+                Quaternion playerRotationAR = GameManager.Instance.playerOrientation * Quaternion.Euler(0, 180f, 0);
+
+                if (isTownhall)
+                {
+                    transform.rotation = (GameManager.Instance.arAnchorOrientation);//Quaternion.Inverse(playerRotationAR) * GameManager.Instance.arAnchorOrientation;
+                }
+                else
+                {
+                    transform.rotation = Quaternion.Inverse(GameManager.Instance.arAnchorOrientation);
+                }
+
+                transform.position = Vector3.Scale(new Vector3(absPos.x, 1.3f, absPos.z), scale_real_to_map);
             }
             else
             {
