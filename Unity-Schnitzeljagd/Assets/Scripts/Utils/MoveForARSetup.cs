6e9a6e80f3c6af97c3fb1c6589a4a1c49d566b60
Konstantin Karas <konstantin.karas@tum.de>
2022-02-11 09:57:30 +0100

    hardcode option

--- Unity-Schnitzeljagd/Assets/Scripts/Utils/MoveForARSetup.cs
+++ Unity-Schnitzeljagd/Assets/Scripts/Utils/MoveForARSetup.cs
@@ -9,9 +9,12 @@ using UnityEngine.SceneManagement;
 public class MoveForARSetup : MonoBehaviour
 {
     private bool isTownhall = false; 
+    private float hardCodeDist = 10f; 
     private Vector3 scale_real_to_map = new Vector3(-2f, 1f, -2f);
     void Awake()
     {
+        bool trialMode = true;
+
         isTownhall = SceneManager.GetActiveScene().Equals(SceneManager.GetSceneByName(Schnitzelconstants.OLD_TOWNHALL_SCENE));
         Debug.Log(isTownhall);
 
@@ -24,16 +27,20 @@ public class MoveForARSetup : MonoBehaviour
         {
             if (GameManager.Instance.useAbsolutePos)
             {
-                transform.position = Vector3.Scale(new Vector3(absPos.x, 1.3f, absPos.z), scale_real_to_map);
-                if (isTownhall)
-                {
-                    transform.rotation = Quaternion.Inverse(GameManager.Instance.playerOrientation);
-                }
-                else
-                {
-                    Vector3 playerRot = GameManager.Instance.playerOrientation.eulerAngles;
-                    transform.Rotate(new Vector3(0f, playerRot.y + 180f, 0f)); // player's y direction is inverted on mapbox!
-                }
+                    transform.position = trialMode ? new Vector3(0f, 1.6f, 0f) : Vector3.Scale(new Vector3(absPos.x, 1.6f, absPos.z), scale_real_to_map);
+                    if (isTownhall)
+                    {
+                    if (!trialMode)
+                    {
+                        transform.rotation = GameManager.Instance.playerOrientation;// Quaternion.Inverse(GameManager.Instance.playerOrientation);
+                    }
+                    }
+                    else
+                    {
+                        Vector3 playerRot = GameManager.Instance.playerOrientation.eulerAngles;
+                        transform.Rotate(new Vector3(0f, playerRot.y + 180f, 0f)); // player's y direction is inverted on mapbox!
+                    }
+                
             }
         }else
         {
@@ -46,18 +53,28 @@ public class MoveForARSetup : MonoBehaviour
 
                 if (isTownhall)
                 {
-                    transform.rotation = (GameManager.Instance.arAnchorOrientation);//Quaternion.Inverse(playerRotationAR) * GameManager.Instance.arAnchorOrientation;
+                    if (!trialMode)
+                    {
+                        transform.rotation = (GameManager.Instance.arAnchorOrientation);//Quaternion.Inverse(playerRotationAR) * GameManager.Instance.arAnchorOrientation;
+                        transform.position = Vector3.Scale(new Vector3(absPos.x, 0f, absPos.z), scale_real_to_map);
+                    }
+                    else
+                    {
+                        transform.position = new Vector3(0f, 0f, hardCodeDist);
+                    }
                 }
                 else
                 {
                     transform.rotation = Quaternion.Inverse(GameManager.Instance.arAnchorOrientation);
+                    transform.position = Vector3.Scale(new Vector3(absPos.x, 0f, absPos.z), scale_real_to_map);
+
                 }
 
-                transform.position = Vector3.Scale(new Vector3(absPos.x, 1.3f, absPos.z), scale_real_to_map);
+
             }
             else
             {
-                transform.position = Vector3.Scale(new Vector3(relativePos.x, -1.3f, relativePos.z), scale_real_to_map);
+                transform.position = Vector3.Scale(new Vector3(relativePos.x, -1.6f, relativePos.z), scale_real_to_map);
                 //transform.rotation = GameManager.Instance.arAnchorOrientation;
             }
         }
